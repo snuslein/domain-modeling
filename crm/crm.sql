a71e58f112f7c5ba27eec3c3e41f01907687a26c
snuslein <sarah.nuslein@kellogg.northwestern.edu>
2023-01-18 01:46:05 +0000

    Class 3 Work

--- crm/crm.sql
+++ crm/crm.sql
@@ -3,3 +3,10 @@
 -- e.g. DROP TABLE IF EXISTS ______;
 
 -- CREATE TABLES
+
+CREATE TABLE salespeople (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
--- school/school.sql
+++ school/school.sql
@@ -16,4 +16,30 @@ CREATE TABLE students (
 
 -- Create the rest of the tables
 
+ CREATE TABLE teachers (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  bio TEXT
+  )
+;
+CREATE TABLE courses (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT,
+  descriptions TEXT
+  )
+  ;
+
+CREATE TABLE sections (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  time TEXT,
+  course_id TEXT,
+  teacher_id TEXT
+);
+
+CREATE enrollments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  section_id  TEXT,
+  student_id TEXT
+);
 
